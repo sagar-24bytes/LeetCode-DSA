91943ceb6752b8607a4946bf9ceb39aa4b861bef
Sagar Chandra <sagarchandra1824@gmail.com>
2026-07-21 10:17:28 +0530

    Time: 12 ms (83.32%) | Memory: 12.6 MB (91.70%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,9 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        st=""
-        for x in s:
-            if x.isalnum():
-                st+=x.lower()
-        return st==st[::-1]
+        left=0
+        right=len(s)-1
+        while left<right:
+            while left<right and not s[left].isalnum():
+                left+=1
+            while left<right and not s[right].isalnum():
+                right-=1
+            if s[left].lower()!=s[right].lower():
+                return False
+            left+=1
+            right-=1
+        return True
         
